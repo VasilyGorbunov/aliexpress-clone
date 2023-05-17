031c1954e35d595357f9f244315260e0546377ec
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-05-17 14:42:02 +0300

    13:00

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div></div>
+  <MainLayout>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      IndexPage
+    </div>
+  </MainLayout>
 </template>
 
 <script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+
 
 </script>
