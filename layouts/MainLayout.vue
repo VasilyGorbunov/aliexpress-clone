3d78cb2164812b2799825e3dce3ea7d347c4747a
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-05-17 17:11:10 +0300

    29:14

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -124,16 +124,77 @@
                 </NuxtLink>
               </div>
             </div>
-
           </div>
         </div>
+
+        <NuxtLink
+          to="/shoppingcart"
+          class="flex items-center"
+        >
+          <button
+            @mouseenter="isCartHover = true"
+            @mouseleave="isCartHover = false"
+            class="relative md:block hidden"
+          >
+            <span
+              class="
+                absolute
+                flex
+                items-center
+                justify-center
+                -right-[3px]
+                top-0
+                bg-[#FF4646]
+                h-[17px]
+                min-w-[17px]
+                text-xs
+                text-white
+                px-0.5
+                rounded-full
+              "
+            >
+              0
+            </span>
+            <div class="min-w-[40px]">
+              <Icon
+                name="ph:shopping-cart-simple-light"
+                size="33"
+                :color="isCartHover ? '#FF4646' : ''"
+              />
+            </div>
+          </button>
+        </NuxtLink>
+
+        <button
+          @click="userStore.isMenuOverlay = true"
+          class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
+        >
+          <Icon
+            name="radix-icons:hamburger-menu"
+            size="33"
+          />
+        </button>
+
       </div>
     </div>
   </div>
+
+  <Loading v-if="userStore.isLoading"/>
+
+  <div class="lg:pt-[150px] md:pt-[130px] pt-[80px]"/>
+  <slot/>
+  <Footer v-if="!userStore.isLoading"/>
 </template>
 
 <script setup>
+import Loading from '../components/Loading.vue'
+import Footer from '../components/Footer.vue'
+
+import {useUserStore} from '~/stores/user';
+const userStore = useUserStore();
+
 let isAccountMenu = ref(false)
+let isCartHover = ref(false)
 let isisSearching = ref(false)
 let searchItem = ref('')
 </script>
