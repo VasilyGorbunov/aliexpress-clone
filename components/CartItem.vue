5b2dfcd02a0ec21351c914c2dc3c0b4ad4979347
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-05-19 15:32:19 +0300

    49:51

---
+++ components/CartItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex justify-start my-2">
+    {{ product }}
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+
+const props = defineProps(['product', 'selectedArray']);
+const { product, selectedArray } = toRefs(props);
+
+const emit = defineEmits(['selectedRadio']);
+
+const isHover = ref(false);
+const isSelected = ref(false);
+
+const remoceFromCart = () => {
+  userStore.cart.forEach((prod, index) => {
+    if (prod.id === product.value.id) {
+      userStore.cart.splice(index, 1);
+    }
+  });
+};
+</script>
--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -21,13 +21,70 @@
             </div>
           </div>
         </div>
+
+        <div class="md:w-[60%] bg-white p-3 rounded-lg">
+          <div v-if="true">
+            <p class="mb-2">Title</p>
+            <p class="font-light text-[12px] mb-2">Description Section</p>
+          </div>
+          <div class="flex items-center pt-1.5">
+            <span class="h-4 min-w-4 rounded-full p-0.5 bg-[#FFD000] mr-2">
+              <Icon name="material-symbols:star-rounded" class="-mt-[17px]" size="12"/>
+            </span>
+            <p class="text-[#FF5353]">Extra 5% off</p>
+          </div>
+          <div class="flex items-center justify-start my-2">
+            <Icon name="ic:baseline-star" color="FF5353"/>
+            <Icon name="ic:baseline-star" color="FF5353"/>
+            <Icon name="ic:baseline-star" color="FF5353"/>
+            <Icon name="ic:baseline-star" color="FF5353"/>
+            <Icon name="ic:baseline-star" color="FF5353"/>
+            <span class="text-[13px] font-light ml-2">5 213 reviews 1,000+ orders</span>
+          </div>
+          <div class="border-b"/>
+          <div class="flex items-center justify-start gap-2 my-2">
+            <div class="text-xl font-bold">${{ priceComputed }}</div>
+            <span class="bg-[#F5F5F5] border text-[#C08562] text-[9px] font-semibold px-1.5 rounded-sm">70% off</span>
+          </div>
+          <p class="text-[#009A66] text-xs font-semibold pt-1">
+            Free 11-day delivery over $8.28
+          </p>
+          <p class="text-[#009A66] text-xs font-semibold pt-1">
+            Free Shipping
+          </p>
+          <div class="py-2"></div>
+
+          <button
+            @click="addToCart()"
+            :disabled="isInCart"
+            class="
+              px-6
+              py-2
+              rounded-lg
+              text-white
+              font-semibold
+              bg-gradient-to-r
+              from-[#FF851A]
+              to-[#FFAC2C]
+            "
+          >
+            <div v-if="isInCart">Is Added</div>
+            <div v-else>Add to Cart</div>
+          </button>
+
+        </div>
+
       </div>
     </div>
   </MainLayout>
 </template>
 
 <script setup>
+import { routeLocationKey } from 'vue-router';
 import MainLayout from '~/layouts/MainLayout.vue';
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+const route = useRoute();
 
 let currentImage = ref(null);
 
@@ -38,6 +95,20 @@ onMounted(() => {
   });
 });
 
+const isInCart = computed(() => {
+  let res = false;
+  userStore.cart.forEach(prod => {
+    if(route.params.id == prod.id) {
+      res = true
+    }
+  });
+  return res;
+});
+
+const priceComputed = computed(() => {
+  return '26.40';
+});
+
 const images = ref([
   '',
   'https://picsum.photos/id/212/800/800',
@@ -46,6 +117,10 @@ const images = ref([
   'https://picsum.photos/id/99/800/800',
   'https://picsum.photos/id/144/800/800',
 ]);
+
+const addToCart = () => {
+  alert('ADDED');
+}
 
 
 </script>
